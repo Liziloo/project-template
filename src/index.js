0c1b1c4cc138773368b6792ea5e58a15864afbc9
Liz <162842106+Liziloo@users.noreply.github.com>
2026-01-06 13:30:07 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} name
+ * @param {number} id
+ */
+function initializeUser(name, id) {
+  console.log(`User ${name} logged in with ID: ${id}`);
+}
+
+// Hover over 'initializeUser' below—VS Code will now show you 
+// exactly what it needs, thanks to the Bridge!
+initializeUser("Geordi", 101);
