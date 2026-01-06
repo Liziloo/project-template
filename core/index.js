bd8845704594b402d1eef64c5b7c59b51c088f34
Liz <162842106+Liziloo@users.noreply.github.com>
2026-01-06 13:27:13 -0500

    Delete core directory

--- core/index.js
+++
@@ -1,8 +0,0 @@
-// Entry point for domain logic.
-//
-// Adapters should call functions exported from this file.
-// This file should not know who is calling it or why.
-
-export function run(input, options = {}) {
-  throw new Error("Not implemented: define core behavior");
-}
